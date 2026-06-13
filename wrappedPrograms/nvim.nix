5b1c86faae3730e531d5bf4bd312c74f1a822ba1
JumpIn-Git <yaasirali0@hotmail.com>
2026-06-13 21:06:48 +0200

    fix workflow

---
+++ wrappedPrograms/nvim.nix
@@ -0,0 +1,16 @@
+{
+  flake.wrappers.nvim = {
+    wlib,
+    config,
+    pkgs,
+    ...
+  }: {
+    imports = [wlib.wrapperModules.neovim];
+    config.specs.plugins.data = with pkgs.vimPlugins; [
+      blink-cmp
+      mini-files
+      mini-icons
+      mini
+    ];
+  };
+}
