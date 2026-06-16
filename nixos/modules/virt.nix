0ca764eb9a0cd5365b389d752ce77bbb0095964a
JumpIn-Git <yaasirali0@hotmail.com>
2026-06-16 21:56:12 +0200

    test a imroved workflow and remove waydroid

--- nixos/modules/virt.nix
+++ nixos/modules/virt.nix
@@ -4,8 +4,6 @@
       enable = true;
       dockerCompat = true;
     };
-    virtualisation.waydroid.enable = true;
-    virtualisation.waydroid.package = pkgs.waydroid-nftables;
 
     environment.etc."distrobox/distrobox.conf".text = ''
       container_additional_volumes="/nix/store:/nix/store:ro /run/current-system:/run/current-system:ro /run/wrappers:/run/wrappers:ro"
@@ -14,7 +12,6 @@
     environment.systemPackages = with pkgs; [
       distrobox
       qemu
-      waydroid-helper
     ];
   };
 }
