970b28212a10c12a61ba4447ee8b37a991d89bf5
JumpIn-Git <yaasirali0@hotmail.com>
2026-08-06 18:37:24 +0200

    tmp

--- wrappedPrograms/noctalia.nix
+++ wrappedPrograms/noctalia.nix
@@ -5,15 +5,30 @@
     lib,
     config,
     ...
-  }:
-  {
+  }: {
     imports = [wlib.modules.default];
     options.configPath = lib.mkOption {
       type = lib.types.str;
       default = toString ../config/noctalia;
     };
     config.package = pkgs.noctalia-shell;
-    config.runtimePkgs = with pkgs; [mission-center gpu-screen-recorder sqlite];
+    config.runtimePkgs = with pkgs; [
+      mission-center
+      gpu-screen-recorder
+      sqlite
+
+      grim
+      slurp
+      wl-clipboard
+      tesseract
+      imagemagick
+      translate-shell
+      wl-screenrec
+      ffmpeg
+      jq
+      python3
+      python3Packages.pygobject3
+    ];
     config.env.NOCTALIA_CONFIG_DIR = config.configPath;
   };
 }
